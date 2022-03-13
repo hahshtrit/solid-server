e9359ded27678fe62b05aba901cf8ed4f430d0f4
Tirthshah <tirthshah571@gmail.com>
2022-03-12 20:08:30 -0500

    trying pull

--- database.py
+++ database.py
@@ -4,6 +4,9 @@ from pymongo import MongoClient
 class database:
     mongo_client = MongoClient("mongo")
     db = mongo_client["cse312"]
+    users = db['users']
+    comments = db['comments']
 
     def __init__(self, request):
         self.request = request
+
--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 import TCPsocket
 import json
 
-#need to add more parse for webbody and things
+
 def split_request(request):
     first_new_line = request.find(parse.new_line)
     blank_line = request.find(parse.blank_line)
@@ -34,3 +34,91 @@ class parse:
         [self.method, self.path, self.http_version] = parse_request_line(request_line)
         self.headers = parse_headers(headersBytes)
 
+    """
+    parseWebKit: takes in the headers from the parse class, and parses the multiform data
+                 to find the WebKit id that is sent in the headers
+    
+    """
+
+    def parseWebKit(self):
+        dict = self.headers
+        if "Content-Length" in dict:
+            type = (dict["Content-Type"])
+            if 'multipart/form-data;' in type:
+                type2 = type.split("boundary=")
+                webStuff = (type2[1])
+                webStuff = '--' + webStuff
+                return webStuff
+        return None
+
+    """
+    ParseWebBody: calls the parseWebkit function, and splits the body of the request from the webkit
+                  and separates out each section. This is generic so you can use the function and 
+                  parse more data in a similar syntax that is being used. It adds all the data in a dictionary
+                  and returns it. This only works if the data contains a webkit which is common when sent a post 
+                  request.
+    """
+
+    def parseWebBody(self):
+        webKit = self.parseWebKit()
+        semi_delimiter = b'\r\n'
+        delimiter = b'\r\n\r\n'
+
+        statements = {}
+
+        if webKit:
+            data2 = self.body.split(webKit.encode())
+
+            for i in range(len(data2)):
+                if 'Content-Disposition: form-data; name="xsrf_token"'.encode() in data2[i]:
+                    current = data2[i]
+                    index = current.find(delimiter)
+                    header = current[:index]
+                    headerIndex = header.find(semi_delimiter)
+                    header = header[headerIndex:]
+                    content = current[index + len(delimiter):]
+                    contentIndex = content.rfind(b'\r\n')
+                    content = content[:contentIndex]
+                    statements['token'] = content
+                    if len(content) == 0:
+                        statements['token'] = b'invalid token accessed'
+
+                if 'Content-Disposition: form-data; name="comment"'.encode() in data2[i]:
+                    current = data2[i]
+                    index = current.find(delimiter)
+                    header = current[:index]
+                    headerIndex = header.find(semi_delimiter)
+                    header = header[headerIndex:]
+                    content = current[index + len(delimiter):]
+                    contentIndex = content.rfind(b'\r\n')
+                    content = content[:contentIndex]
+                    statements['comment'] = content
+
+                if 'Content-Disposition: form-data; name="upload"'.encode() in data2[i]:
+                    current = data2[i]
+                    index = current.find(delimiter)
+                    header = current[:index]
+                    headerIndex = header.find(semi_delimiter)
+                    header = header[headerIndex:]
+                    header = header.split(b';')
+
+                    parse1 = (header[2].split(semi_delimiter))
+
+                    fileNameIndex = parse1[0].find(b'=')
+                    fileName = ((parse1[0])[fileNameIndex + len(b'='):]).decode()
+                    fileName = fileName.replace("/", "")
+
+                    contentTypeIndex = parse1[1].find(b': ')
+                    contentType = ((parse1[1])[contentTypeIndex + len(b': '):]).decode()
+
+                    content = current[index + len(delimiter):]
+                    contentIndex = content.rfind(semi_delimiter)
+                    content = content[:contentIndex]
+
+                    if "image/jpeg" in contentType:
+                        if len(content) > 0:
+                            statements['bytes'] = content
+                            statements['filename'] = fileName
+                            statements['contentType'] = contentType
+
+        return statements
