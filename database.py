8c67908b769a45c03959f4e6f7e75a358e775e51
Tirth Shah <65323068+hahshtrit@users.noreply.github.com>
2022-04-23 15:08:07 -0400

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-class database:
-    mongo_client = MongoClient("mongo")
-    db = mongo_client["cse312"]
-
-    def __init__(self, request):
-        self.request = request
