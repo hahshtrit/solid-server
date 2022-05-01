9c938943bd26b2ef54453201d4d7a7138b4528ee
Vedant Ombase <vedantom@buffalo.edu>
2022-05-01 17:30:21 -0400

    database classes are unnecessary

--- database.py
+++ database.py
@@ -1,9 +1,4 @@
 from pymongo import MongoClient
 
-
-class database:
-    mongo_client = MongoClient("mongo")
-    db = mongo_client["cse312"]
-
-    def __init__(self, request):
-        self.request = request
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse312"]
