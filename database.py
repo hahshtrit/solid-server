ab3ad4ffee45be388c1bdc28bb78bdd63d559ea6
Abi Abisha <syahrial.abisha@gmail.com>
2022-05-08 17:36:53 -0400

    fix

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
-from utils.authentication import hash_password
+from utils.authentication import hash_password, create_auth_token
+import bcrypt
 
 mongo_client = MongoClient("mongo")
 db = mongo_client["cse312"]
 
-# db should be
+# db['userinfo'] should be
 # {
 # username: _               (in bytes)
 # hash_salt_password: _     (in bytes)
@@ -14,3 +15,12 @@ db = mongo_client["cse312"]
 def register_user(entered_username: str, entered_password: str):
     # hash+salt pass, store
     pass
+
+def validate_login(entered_username: str, entered_password: str):
+    return False
+
+# db['messages'] should be
+# {
+# username: _               (in bytes)
+# message: _                (in bytes)  <- this is multimedia content
+# }
