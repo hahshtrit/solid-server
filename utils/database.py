c4355f72eae3de8cdce7d77c61e5a0f763b6b58f
Vedant Ombase <vedantom@buffalo.edu>
2022-05-08 22:45:39 -0400

    Working on authentication

--- utils/database.py
+++ utils/database.py
@@ -5,6 +5,11 @@ import bcrypt
 mongo_client = MongoClient("mongo")
 db = mongo_client["cse312"]
 
+userinfo = db["userinfo"]
+
+
+# username could be in plaintext
+
 # db['userinfo'] should be
 # {
 # username: _               (in bytes)
@@ -12,12 +17,21 @@ db = mongo_client["cse312"]
 # hash_salt_auth_token: _   (in bytes)
 # }
 
-def register_user(entered_username: str, entered_password: str):
-    # hash+salt pass, store
-    pass
+def register_user(entered_username: str, entered_password: str, auth_token: str = ""):
+    userinfo.insert_one({"username": entered_username, "password": hash_password(entered_password), "auth_token": b""})
 
-def validate_login(entered_username: str, entered_password: str):
-    return False
+
+# only need to query a user by username or auth_token
+def find_user(username: str = "", auth_token: str = "") -> bool:
+    if not username and not auth_token:
+        print("At least one argument should be given")
+        # and also, this stops exploits from getting access using empty auth_tokens
+        return False
+
+    if username:
+        return True if userinfo.find_one({"username": username}, {"_id": 0}) else False
+    elif auth_token:
+        return True if userinfo.find_one({"auth_token": hash_token(auth_token)}, {"_id": 0}) else False
 
 # db['messages'] should be
 # {
