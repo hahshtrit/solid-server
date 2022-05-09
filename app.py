94d6f58b31f3667090ee7b79f4980021d41f58af
Vedant Ombase <vedantom@buffalo.edu>
2022-05-08 21:44:09 -0400

    moved everything out of subdirectory flasking

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import render_template, request, redirect
-from flasking import app
-import database as db
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 @app.route("/")
 @app.route("/home")
 def homepage():
     return render_template('homepage.html')
 
-@app.route("/about")    # not gonna be used
+
+@app.route("/about")  # not gonna be used
 def about():
     return render_template('about.html')
+
 
 @app.route("/account")
 def account():
     return render_template('account.html')
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -25,6 +32,7 @@ def login():
         return redirect("/")
     return render_template('login.html')
 
+
 @app.route("/sign-up", methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
@@ -32,6 +40,6 @@ def signup():
         password = request.form.get("password")
         print("signup")
         print(f"username: {username}, password: {password}")
-        
+
         return redirect("/")
     return render_template('signup.html')
